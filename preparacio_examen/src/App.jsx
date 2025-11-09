5a738a98748e38e85abb09c86941d83b038cf4b3
Silvia <silvia.serra.7e6@itb.cat>
2025-11-09 23:05:16 +0100

    add exemple formulari

---
+++ preparacio_examen/src/App.jsx
@@ -0,0 +1,10 @@
+import FormulariEnComponent from "./components/FormulariEnComponent";
+
+export default function App() {
+  return (
+    <div className="app-container">
+      <h1>Formulari d'exemple</h1>
+      <FormulariEnComponent />
+    </div>
+  );
+}
